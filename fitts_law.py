68201e373d26c81dbf9128c76f8339ca45db4997
He Buwei <hebuweitom@gmail.com>
2018-04-26 03:06:29 +0800

    code cleanup

--- fitts_law.py
+++ fitts_law.py
@@ -3,7 +3,47 @@ from scipy import optimize
 
 
 P_THRESHOLD = 0.1
-LOG_TP_PRECISION = 1e-7
+
+TP_MIN = 0.1
+TP_MAX = 100
+
+
+# calculates the throughput required to fc the map with probability P_THRESHOLD
+def calc_throughput(Ds_MTs, W):
+
+    fc_prob_TP_min = calc_fc_prob(TP_MIN, Ds_MTs, W)
+    
+    # if map is so easy that players with minimum throughput can fc with decent possibility
+    if fc_prob_TP_min >= P_THRESHOLD:
+        return TP_MIN
+
+    fc_prob_TP_max = calc_fc_prob(TP_MAX, Ds_MTs, W)
+
+    # if map is too hard 
+    if fc_prob_TP_max <= P_THRESHOLD:
+        return TP_MAX
+
+    # x, r = optimize.brentq(calc_fc_prob_minus_threshold, TP_MIN, TP_MAX, args=(Ds_MTs, W), full_output=True)
+    # print(r.iterations)
+
+    x = optimize.brentq(calc_fc_prob_minus_threshold, TP_MIN, TP_MAX, args=(Ds_MTs, W))
+
+    return x
+
+
+def calc_fc_prob_minus_threshold(TP, Ds_MTs, W):
+    return calc_fc_prob(TP, Ds_MTs, W) - P_THRESHOLD
+    
+
+def calc_fc_prob(TP, Ds_MTs, W):
+    fc_prob = 1.0
+
+    for (D, MT) in Ds_MTs:
+        hit_prob = calc_hit_prob(D, W, MT, TP)
+        fc_prob *= hit_prob
+
+    # print('' + str(TP) + ' | ' + str(fc_prob))
+    return fc_prob
 
 
 # calculates the probability a player with throughput TP can hit an object
@@ -22,58 +62,6 @@ def calc_hit_prob(D, W, MT, TP):
         return 0.0
 
     return math.erf(2.066 * (W/D) * (2**((MT-a)*TP)-1) / 2**0.5)
-
-
-def calc_fc_prob(TP, Ds_MTs, W):
-    fc_prob = 1.0
-
-    for (D, MT) in Ds_MTs:
-        hit_prob = calc_hit_prob(D, W, MT, TP)
-        fc_prob *= hit_prob
-
-    # print('' + str(TP) + ' | ' + str(fc_prob))
-    return fc_prob
-
-
-def calc_throughput(Ds_MTs, W):
-    log_TP_min = -3.0
-    log_TP_max = 5.0
-
-    fc_prob_TP_min = calc_fc_prob(math.exp(log_TP_min), Ds_MTs, W)
-    
-    # if map is so easy that players with minimum throughput can fc with decent possibility
-    if fc_prob_TP_min >= P_THRESHOLD:
-        return math.exp(log_TP_min)
-
-    fc_prob_TP_max = calc_fc_prob(math.exp(log_TP_max), Ds_MTs, W)
-
-    # if map is too hard 
-    if fc_prob_TP_max <= P_THRESHOLD:
-        return math.exp(log_TP_max)
-
-    # # binary search for a throughput that corresponds to an fc probability of P_THRESHOLD
-    # while log_TP_max - log_TP_min > LOG_TP_PRECISION:
-        
-    #     log_TP_mid = (log_TP_min + log_TP_max) / 2
-        
-    #     prob = calc_fc_prob(Ds_MTs, W, math.exp(log_TP_mid))
-
-    #     if prob > P_THRESHOLD:
-    #         log_TP_max = log_TP_mid
-    #     else:
-    #         log_TP_min = log_TP_mid
-
-    #     count = count + 1
-
-    def fc_prob_minus_threshold(TP, Ds_MTs, W):
-        return calc_fc_prob(TP, Ds_MTs, W) - P_THRESHOLD
-
-    x, r = optimize.brentq(fc_prob_minus_threshold, math.exp(log_TP_min), math.exp(log_TP_max), args=(Ds_MTs, W), full_output=True)
-
-    print(r)
-
-    # return math.exp((log_TP_min + log_TP_max) / 2)
-    return x
 
 
 def calc_IP(D, W, MT):
