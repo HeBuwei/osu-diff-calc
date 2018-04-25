9c4ce4ac721f7d4f5d076229f87f33b951cc8534
He Buwei <hebuweitom@gmail.com>
2018-04-26 02:24:09 +0800

    use a better root finding algorithm to find throughput

--- fitts_law.py
+++ fitts_law.py
@@ -1,4 +1,6 @@
 import math
+from scipy import optimize
+
 
 P_THRESHOLD = 0.1
 LOG_TP_PRECISION = 1e-7
@@ -22,7 +24,7 @@ def calc_hit_prob(D, W, MT, TP):
     return math.erf(2.066 * (W/D) * (2**((MT-a)*TP)-1) / 2**0.5)
 
 
-def calc_fc_prob(Ds_MTs, W, TP):
+def calc_fc_prob(TP, Ds_MTs, W):
     fc_prob = 1.0
 
     for (D, MT) in Ds_MTs:
@@ -37,31 +39,41 @@ def calc_throughput(Ds_MTs, W):
     log_TP_min = -3.0
     log_TP_max = 5.0
 
-    fc_prob_TP_min = calc_fc_prob(Ds_MTs, W, math.exp(log_TP_min))
+    fc_prob_TP_min = calc_fc_prob(math.exp(log_TP_min), Ds_MTs, W)
     
     # if map is so easy that players with minimum throughput can fc with decent possibility
-    if fc_prob_TP_min > P_THRESHOLD:
+    if fc_prob_TP_min >= P_THRESHOLD:
         return math.exp(log_TP_min)
 
-    fc_prob_TP_max = calc_fc_prob(Ds_MTs, W, math.exp(log_TP_max))
+    fc_prob_TP_max = calc_fc_prob(math.exp(log_TP_max), Ds_MTs, W)
 
     # if map is too hard 
-    if fc_prob_TP_max < P_THRESHOLD:
+    if fc_prob_TP_max <= P_THRESHOLD:
         return math.exp(log_TP_max)
 
-    # binary search for a throughput that corresponds to an fc probability of P_THRESHOLD
-    while log_TP_max - log_TP_min > LOG_TP_PRECISION:
+    # # binary search for a throughput that corresponds to an fc probability of P_THRESHOLD
+    # while log_TP_max - log_TP_min > LOG_TP_PRECISION:
         
-        log_TP_mid = (log_TP_min + log_TP_max) / 2
+    #     log_TP_mid = (log_TP_min + log_TP_max) / 2
         
-        prob = calc_fc_prob(Ds_MTs, W, math.exp(log_TP_mid))
+    #     prob = calc_fc_prob(Ds_MTs, W, math.exp(log_TP_mid))
 
-        if prob > P_THRESHOLD:
-            log_TP_max = log_TP_mid
-        else:
-            log_TP_min = log_TP_mid
+    #     if prob > P_THRESHOLD:
+    #         log_TP_max = log_TP_mid
+    #     else:
+    #         log_TP_min = log_TP_mid
 
-    return math.exp((log_TP_min + log_TP_max) / 2)
+    #     count = count + 1
+
+    def fc_prob_minus_threshold(TP, Ds_MTs, W):
+        return calc_fc_prob(TP, Ds_MTs, W) - P_THRESHOLD
+
+    x, r = optimize.brentq(fc_prob_minus_threshold, math.exp(log_TP_min), math.exp(log_TP_max), args=(Ds_MTs, W), full_output=True)
+
+    print(r)
+
+    # return math.exp((log_TP_min + log_TP_max) / 2)
+    return x
 
 
 def calc_IP(D, W, MT):
